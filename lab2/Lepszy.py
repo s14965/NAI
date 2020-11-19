1daa7b6dea168609c0d68ca5600d7b008297e5ae
s14965 <s14965@pjwstk.edu.pl>
2020-11-19 14:56:09 +0100

    lab2, kolko krzyzyk

---
+++ lab2/Lepszy.py
@@ -0,0 +1,130 @@
+#Tic Tac Toe game in python
+
+board = [' ' for x in range(10)]
+
+def insertLetter(letter, pos):
+    board[pos] = letter
+
+def spaceIsFree(pos):
+    return board[pos] == ' '
+
+def printBoard(board):
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    
+def isWinner(bo, le):
+    return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or(bo[1] == le and bo[2] == le and bo[3] == le) or(bo[1] == le and bo[4] == le and bo[7] == le) or(bo[2] == le and bo[5] == le and bo[8] == le) or(bo[3] == le and bo[6] == le and bo[9] == le) or(bo[1] == le and bo[5] == le and bo[9] == le) or(bo[3] == le and bo[5] == le and bo[7] == le)
+
+def playerMove():
+    run = True
+    while run:
+        move = input('Wybierz pole dla \'X\'a (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('To pole jest zajete')
+            else:
+                print('Podaj pole z zakresu')
+        except:
+            print('Wpisz liczbe od 1-9')
+            
+
+def compMove():
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1,3,7,9]:
+            cornersOpen.append(i)
+   
+    if 5 in possibleMoves:
+        move = 5
+        return move
+        
+        
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+
+   
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+            
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        
+    return move
+
+def selectRandom(li):
+    import random
+    return random.choice(li)
+
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def main():
+    print('Kolko i krzyzyk')
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print('AI wygrywa!')
+            break
+
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Remis!')
+            else:
+                insertLetter('O', move)
+                print('AI umiescilo \'O\' na polu', move , ':')
+                printBoard(board)
+        else:
+            print('X wygral!')
+            break
+
+    if isBoardFull(board):
+        print('Remis!')
+
+
+main()
+while True:
+    answer = input('Graj ponwonie? (Y/N)')
+    if answer.lower() == 'y' or answer.lower == 'yes':
+        board = [' ' for x in range(10)]
+        print('-----------------------------------')
+        main()
+    else:
+        break
